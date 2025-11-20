9a3002ed1c4a8ed5ef9be216b5d42a83105bd62f
Dawson Pitcher <dawsonlpitcher@gmail.com>
2025-11-20 15:43:23 -0700

    update db.js for elastic beanstalk RDS

--- util/db.js
+++ util/db.js
@@ -2,16 +2,16 @@
 require('dotenv').config();
 
 // Knex configuration: creates a reusable database connection for the app
-const knex = require('knex')({
-    client: 'pg',
+const knex = require("knex")({
+    client: "pg",
     connection: {
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        port: process.env.DB_PORT
+        host: process.env.RDS_HOSTNAME || process.env.DB_HOST,
+        user: process.env.RDS_USERNAME || process.env.DB_USER,
+        password: process.env.RDS_PASSWORD || process.env.DB_PASSWORD,
+        database: process.env.RDS_DB_NAME || process.env.DB_NAME,
+        port: process.env.RDS_PORT || process.env.DB_PORT,
     }
 });
-
 // Export the configured Knex instance so models can run queries
 module.exports = knex;
+
